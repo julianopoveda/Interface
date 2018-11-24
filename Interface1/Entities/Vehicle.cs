71cf5149fa0ae721ebadd96ae67c302d1d8c4882
caiomodena <caio.modena@gmail.com>
2018-11-24 18:10:10 -0200

    finalizando sem utilizar interfaces

---
+++ Interface1/Entities/Vehicle.cs
@@ -0,0 +1,9 @@
+using System;
+namespace Entities{
+    class Vehicle{
+        public string Model { get; set; }
+        public Vehicle(string model){
+            this.Model=model;
+        }
+    }
+}
---
+++ Interface1/Services/BrazilTaxService.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Services{
+    class BrazilTaxServices{
+        public double tax(double amount){
+            if(amount<=100.00){
+                return amount*0.2;
+            }
+            else{
+                return amount * 0.15;
+            }
+        }
+
+    }
+}
