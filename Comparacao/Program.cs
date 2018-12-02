ab9fdd15b394387ffa9bb23d0143fcf6f6ed04e7
caiomodena <caio.modena@gmail.com>
2018-12-02 11:44:02 -0200

    exercicio de comparacao

---
+++ Comparacao/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Comparacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminho=@"C:\Users\caio\Desktop\Nomes.txt";
+            try{
+                using(StreamReader sr = File.OpenText(caminho)){
+                List<string> lista=new List<string>();
+                while(!sr.EndOfStream){
+                    lista.Add(sr.ReadLine());
+                }
+                lista.Sort();
+                foreach(string str in lista){
+                    System.Console.WriteLine(str);
+                }
+              }
+            }
+            catch(IOException ex){
+                System.Console.WriteLine("Um erro ocorreu");
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
